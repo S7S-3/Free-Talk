6e9c1eea1d2e3d182df4e872718bc329aa32c269
MinaSafwat2411 <102329342+Faswet12@users.noreply.github.com>
2024-12-10 13:20:41 +0200

    develop

--- lib/views/login/login_views.dart
+++ lib/views/login/login_views.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = Provider.of<LoginProvider>(context);
 
+
     return Scaffold(
       backgroundColor: AppColors.gulfBlue,
       resizeToAvoidBottomInset: true, // Ensures layout adjusts for keyboard
